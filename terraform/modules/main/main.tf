4e65788d2e63b0b27a2ed5ae121dc831d5d59092
Yahya Abdul-Halim <yabdulhalim@microsoft.com>
2022-05-10 18:06:22 +0100

    updating to latest version

--- terraform/modules/main/main.tf
+++ terraform/modules/main/main.tf
@@ -14,7 +14,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.5.0"
     }
   }
   required_version = ">= 1.1.0"
