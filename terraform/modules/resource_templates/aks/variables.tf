7ca7e5b3e687a7a9a7d62c6fb0b19ca7bfd24b49
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-22 04:52:45 +0100

    Delete old code

--- terraform/modules/resource_templates/aks/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aks_name" {
-  description = "Name of the azure kubernetes cluster"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-}
